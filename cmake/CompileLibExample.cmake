5c600a1358563f4d731405e18421c2a97a6d7654
Aske B.M <asmoe16@student.sdu.dk>
2020-09-23 11:45:05 +0200

    First pass modern cmake with extern provided lib

---
+++ cmake/CompileLibExample.cmake
@@ -0,0 +1,2 @@
+add_executable(lib_example ${PROJECT_SOURCE_DIR}/extern/lib_example/lib_example.cpp)
+target_link_libraries(lib_example GPIO)
---
+++ cmake/FindGPIO.cmake
@@ -0,0 +1,3 @@
+add_library(GPIO SHARED IMPORTED)
+set_target_properties(GPIO PROPERTIES IMPORTED_LOCATION ${PROJECT_SOURCE_DIR}/extern/lib_example/libGPIO.so)
+target_include_directories(GPIO PUBLIC ${PROJECT_SOURCE_DIR}/extern/lib_example)
