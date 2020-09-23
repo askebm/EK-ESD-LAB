e8129854a24be3a39492e0d0c3c49b2c403c65ab
Aske Bækdal Møller <askebm@live.dk>
2020-09-23 19:33:44 +0200

    updated to lib_example_v2

--- cmake/FindGPIO.cmake
+++ cmake/FindGPIO.cmake
@@ -1,5 +1,3 @@
 add_library(GPIO SHARED IMPORTED)
-set_target_properties(GPIO 
-	PROPERTIES IMPORTED_LOCATION  ${PROJECT_SOURCE_DIR}/extern/lib_example/libGPIO.so
-	INTERFACE_INCLUDE_DIRECTORIES ${PROJECT_SOURCE_DIR}/extern/lib_example
-	)
+set_target_properties(GPIO PROPERTIES IMPORTED_LOCATION  ${PROJECT_SOURCE_DIR}/extern/lib_example/libGPIO.so)
+target_include_directories(GPIO INTERFACE ${PROJECT_SOURCE_DIR}/extern/lib_example)
